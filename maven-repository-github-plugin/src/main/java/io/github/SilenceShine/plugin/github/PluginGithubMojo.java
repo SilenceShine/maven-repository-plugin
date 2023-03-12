48c0a628b28b574a967b2bdb26b73d2835628028
SilenceRainly <1292798418@qq.com>
2023-03-12 21:42:08 +0800

    init.

---
+++ maven-repository-github-plugin/src/main/java/io/github/SilenceShine/plugin/github/PluginGithubMojo.java
@@ -0,0 +1,18 @@
+package io.github.SilenceShine.plugin.github;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @author SilenceShine
+ * @since 1.0
+ */
+public class PluginGithubMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+
+}
